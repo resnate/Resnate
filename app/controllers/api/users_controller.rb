7da76ecf29c5205200d2ff9d198e39ea73ea8f0f
Amir Moosavi <thenametheyallfear@hotmail.com>
2015-05-21 23:58:47 +0100

    no rabl 25

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,6 +10,9 @@ class API::UsersController < ApplicationController
 
   def userSearch
     @profile = JSON.parse(Net::HTTP.get_response(URI.parse("https://graph.facebook.com/me?access_token=" + params[:oauth] + "&appsecret_proof=" + OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, FACEBOOK_CONFIG['secret'], params[:oauth]))).body.html_safe)["id"]
+    if User.find_by_uid(@profile).nil?
+      @profile = nil
+    end
   end
 
 end
