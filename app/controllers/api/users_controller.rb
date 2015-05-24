2d643c1721930dbaf5967ca81a4b754eae377f56
Amir Moosavi <thenametheyallfear@hotmail.com>
2015-05-24 12:06:53 +0100

    add name api fix 3

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -20,7 +20,7 @@ before_filter :restrict_access, :except => :userSearch
     else
       id = User.find_by_uid(@profile).id
       @access_token = APIKey.find_by_user_id(id).access_token
-      @name = User.find(id).name
+      @name = id
     end
   end
 
