c68d7828a5da4e50baa1184d947ccad972458a5e
Amir Moosavi <thenametheyallfear@hotmail.com>
2015-12-04 08:01:51 +0000

    profile changes 12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -145,6 +145,30 @@ end
       @gigs = @user.gigs
       @reviews = @user.reviews
       @review = Review.where(user_id: @user.id).last
+      points = @user.points
+      if points < 5
+        @percentToNextLevel = points/5
+        @pointsToNextLevel = 5 - points
+      elsif points < 10
+        @percentToNextLevel = (points - 5)/5
+        @pointsToNextLevel = 10 - points
+      elsif points < 25
+        @percentToNextLevel = (points - 10)/15
+        @pointsToNextLevel = 25 - points
+      elsif points < 100
+        @percentToNextLevel = (points - 25)/75
+        @pointsToNextLevel = 100 - points
+      elsif points < 500
+        @percentToNextLevel (points - 100)/400
+        @pointsToNextLevel = 500 - points
+      elsif points < 1000
+        @percentToNextLevel = (points - 500)/500
+        @pointsToNextLevel = 1000 - points
+      elsif points < 10000
+        @percentToNextLevel = (points - 1000)/9000
+        @pointsToNextLevel = 10000 - points
+      end
+
       render :layout => false
     end
 
