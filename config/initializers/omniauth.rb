08574190ef3e9b2e0aab625c24db7b52aa452239
Amir Moosavi <thenametheyallfear@hotmail.com>
2016-01-28 12:57:34 +0000

    api user create 15

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,8 @@ OmniAuth.config.on_failure = Proc.new { |env|
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, FACEBOOK_CONFIG['app_id'], ENV['FB_SECRET'],
-  			:image_size => 'large',
-           :scope => 'user_friends,user_likes,email', :provider_ignores_state => true
+  			image_size: 'large',
+           scope: 'user_friends,user_likes,email', provider_ignores_state: true,
+    callback_path: '/auth/facebook/callback'
            #, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
