6cec87972e655738f669f500f6d560af41ff00a3
Amir Moosavi <thenametheyallfear@hotmail.com>
2016-01-28 10:56:07 +0000

    api user create 11

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,7 +4,7 @@ class API::UsersController < ApplicationController
   require 'uri'
   require 'net/http'
   require 'json'
-  
+
   def index
     @users = User.all
   end
@@ -30,7 +30,7 @@ class API::UsersController < ApplicationController
   end
 
   def create
-    @user = JSON.parse(Net::HTTP.get_response(URI.parse("https://graph.facebook.com/me?access_token=" + params[:oauth] + "&appsecret_proof=" + OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, FACEBOOK_CONFIG['secret'], params[:oauth]))).body.html_safe)
+    @user = JSON.parse(Net::HTTP.get_response(URI.parse("https://graph.facebook.com/me?access_token=" + params[:oauth] + "&appsecret_proof=" + OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, FACEBOOK_CONFIG['secret'], params[:oauth]))))
   end
 
   def friendsWhoLike
