8d4f5a2a9504f21f15b60046f02cbf8b05cc4e55
Amir Moosavi <thenametheyallfear@hotmail.com>
2016-02-08 03:04:26 +0000

    add user destroy controller 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,6 @@ class User < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :gigs, dependent: :destroy
   has_many :past_gigs, dependent: :destroy
-  has_one :api_keys, dependent: :destroy
   
 
   serialize :musicLikes, JSON
