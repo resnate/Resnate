4dfdd3abfefc6490884c513253a18be507ce64a4
Amir Moosavi <thenametheyallfear@hotmail.com>
2016-02-19 09:51:24 +0000

    reviews pl mobile fix

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,9 @@
 class Review < ActiveRecord::Base
-  belongs_to :reviewable, polymorphic: true
-  belongs_to :user
-  validates :content, presence: true, length: { maximum: 5000 }
-  validates :user_id, presence: true
-  acts_as_likeable
-  include PublicActivity::Common
+	default_scope -> { order('created_at DESC') }
+  	belongs_to :reviewable, polymorphic: true
+  	belongs_to :user
+  	validates :content, presence: true, length: { maximum: 5000 }
+  	validates :user_id, presence: true
+  	acts_as_likeable
+  	include PublicActivity::Common
 end
