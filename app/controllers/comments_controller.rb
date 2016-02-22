3cf80c05026a052d747ec4ae54e6c23c6b9725c3
Amir Moosavi <thenametheyallfear@hotmail.com>
2016-02-22 11:49:03 +0000

    comments follow notifications

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,41 @@
 class CommentsController < ApplicationController
 
   def create
-  	if params[:commentable_type] == "activity"
-      @commentable = PublicActivity::Activity.find(params[:commentable_id])       
+    recipients = []
+    if params[:commentable_type] == "activity"
+      @commentable = PublicActivity::Activity.find(params[:commentable_id]) 
+      recipient = @commentable.owner_id
+      if @commentable.trackable_type == "Song"    
+        notification = "C|S"
+      elsif @commentable.trackable_type == "Playlist"    
+        notification = "C|P"
+      elsif @commentable.trackable_type == "Gig"    
+        notification = "C|G"
+      end
     elsif params[:commentable_type] == "review"
-  		@commentable = Review.find(params[:commentable_id])
-  	end
+      @commentable = Review.find(params[:commentable_id])
+      recipient = @commentable.user_id
+      notification = "C|R"
+    end
+
+    recipients.push(recipient)
+
     @get = "/activity/" + params[:commentable_id] + "/comments/"
     @comment = Comment.build_from( @commentable, current_user.id, params[:body] )
     @comment.save
     Pusher.trigger('comments', 'comment', {:message => @get})
+
+    Comment.where(commentable_id: @comment.id).each do |c|
+      if c.user_id != @commentable.owner_id && c.user_id != current_user.id
+        recipients.push(c.user_id)
+      end
+    end
+
+
+    current_user.send_message(@recipients, params[:body], notification)
+    @recipients.each do |r|
+      Pusher.trigger('messages', 'inbox', { message: r.id, sender: @sender})
+    end
   	render :layout => false
   end
 
