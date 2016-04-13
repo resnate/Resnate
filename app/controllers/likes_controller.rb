7fc990e93d20a228effcfd07931cf3e0b70796f1
Amir Moosavi <thenametheyallfear@hotmail.com>
2016-04-13 23:19:40 +0100

    big likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,7 @@ class LikesController < ApplicationController
 	def show
       @user = User.find(params[:user])
       @likes = Like.where(liker_id: @user.id, likeable_type: "Song")
+      @firstLike = Song.find(Like.where(liker_id: @user.id, likeable_type: "Song").first.likeable_id).content
       render :layout => false
     end
 
